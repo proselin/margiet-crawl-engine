86eb13ff2cd6a532871655a35c262545fa3b6591
Hung Nguyen Quoc <quochung9401@gmail.com>
2024-09-19 21:28:00 +0700

    feat: Update docker file and health Check

--- src/common/logger/winston/winston-logger.module.ts
+++ src/common/logger/winston/winston-logger.module.ts
@@ -3,27 +3,118 @@ import {
   utilities as nestWinstonModuleUtilities,
   WinstonModule,
 } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
+import { EnvKey } from '@/environment';
+import { Client } from '@elastic/elasticsearch';
 import * as winston from 'winston';
+import * as Elasticsearch from 'winston-elasticsearch';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+
+const esClient = new Client({
+  node: process.env.ELASTICSEARCH_NODE || 'http://localhost:9200',
+  auth: {
+    username: process.env.ELASTICSEARCH_USERNAME || 'elastic',
+    password: process.env.ELASTICSEARCH_PASSWORD || 'elastic',
+  },
+});
+
+const esTransportOpts: Elasticsearch.ElasticsearchTransportOptions = {
+  level: 'info',
+  client: esClient,
+  indexPrefix: 'crawl-engine-logs', // Customize the index name as needed
+};
+
+const winstonConfigProduction: winston.LoggerOptions = {
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(), // Use JSON format for structured logging
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(), // Adds color to console logs
+        nestWinstonModuleUtilities.format.nestLike('CrawlEngine', {
+          prettyPrint: true,
+        }),
+      ),
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    new winston.transports.File({
+      filename: 'logs/combined.log',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    new Elasticsearch.ElasticsearchTransport(esTransportOpts),
+    new DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d', // Retain logs for 14 days
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    new DailyRotateFile({
+      filename: 'logs/error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '30d', // Retain error logs for 30 days
+      level: 'error',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'logs/exceptions.log' }),
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({ filename: 'logs/rejections.log' }),
+  ],
+};
+
+const winstonConfigDevelopment = {
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.ms(),
+        nestWinstonModuleUtilities.format.nestLike('MargietApp', {
+          colors: true,
+          prettyPrint: true,
+          processId: true,
+          appName: true,
+        }),
+      ),
+    }),
+  ],
+};
 
 @Module({
   imports: [
-    WinstonModule.forRoot({
-      transports: [
-        new winston.transports.Console({
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.ms(),
-            nestWinstonModuleUtilities.format.nestLike('MargietApp', {
-              colors: true,
-              prettyPrint: true,
-              processId: true,
-              appName: true,
-            }),
-          ),
-        }),
-        // other transports...
-      ],
-      // other options
+    WinstonModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        if (configService.get(EnvKey.NODE_ENV) !== 'production') {
+          return winstonConfigDevelopment;
+        }
+        return winstonConfigProduction;
+      },
     }),
   ],
 })
