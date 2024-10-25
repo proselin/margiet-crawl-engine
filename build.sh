4944fb74be0511f9ec732c1d5104647d6ae60766
Hung Nguyen Quoc <quochung9401@gmail.com>
2024-10-25 10:27:10 +0700

    build: change build.sh and update package.json

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 rm -rf node_temp
 mkdir  node_temp &&
+cp     -r node_modules node_temp &&
 cp     package.json node_temp &&
 cp     -r libs node_temp/libs &&
 npm    i --prefix node_temp &&
-docker build -t crawl-engine:0.0.1 --progress=plain --platform linux/amd64 . &&
+docker build -t margiet-crawl-engine:0.0.1 --progress=plain --platform linux/amd64 . &&
 rm     -rf node_temp
