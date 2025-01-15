79777d63e0ac38e4071c753ec6d6df1149995a3b
Hung Nguyen Quoc <quochung9401@gmail.com>
2025-01-15 17:36:03 +0700

    Change env produce

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
-import { NODE_ENV } from './common';
-import { configDotenv } from 'dotenv';
 import { envValidation } from './config';
 import redisConfig from './config/redis.config';
 import databaseConfig from './config/database/database.config';
@@ -13,24 +10,6 @@ import { CrawlConsumerModule } from './queues/consumers/craw-consumer';
 import { CrawlProducerModule } from './queues/producers/crawl-producer';
 import { CrawlModule } from './crawl';
 import { RefreshComicModule } from './cronjob/refresh-comic';
-
-function loadEnv() {
-  switch (process.env.NODE_ENV) {
-    case NODE_ENV.PRODUCTION: {
-      configDotenv({
-        path: '.env.prod',
-      });
-    }
-    case NODE_ENV.DEVELOPMENT:
-    default: {
-      configDotenv({
-        path: '.env.local',
-      });
-    }
-  }
-}
-
-loadEnv();
 
 @Module({
   imports: [
