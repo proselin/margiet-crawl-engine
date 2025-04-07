39c769d01c30fc2aeed52cc766a2d61b48a81f54
Hung Nguyen Quoc <quochung9401@gmail.com>
2025-04-07 14:51:43 +0000

    refactor: Remove build.sh file

--- build.sh
+++
@@ -1,2 +0,0 @@
-#docker build -t margiet-crawl-engine:0.0.1 --progress=plain --platform linux/amd64 . --network=host
-docker build -f ./Dockerfile -t crawl-engine-bun:0.0.1 --progress=plain --platform linux/amd64 . --network=host
